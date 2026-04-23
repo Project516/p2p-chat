78f5193ea5e225bb43cd4beeb209a8f16f5f2b5a
project516 <138796702+Project516@users.noreply.github.com>
2026-04-23 14:51:25 +0000

    template go code

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "os"
 
 func main() {
-	fmt.Println("Go")
+
+	args := os.Args
+
+	if (len(args) > 1) {
+		if (args[1] == "listen") {
+			fmt.Println("Listening...")
+		} else if (args[1] == "connect") {
+			fmt.Println("Connecting...")
+		} else {
+			fmt.Println("Usage: go run . [listen|connect] address")
+		}
+	} else {
+		fmt.Println("Usage: go run . [listen|connect] address")
+	}
 }
